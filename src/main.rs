493d331aec3dcba5a2cf0635e2ab80c7d71aa8d5
Austin Crim <aust.crim@gmail.com>
2021-11-03 15:18:23 -0500

    home route, dynamic host

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
+use std::env;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+    let is_prod = match env::var("RENDER") {
+        Ok(_) => true,
+        Err(_) => false,
+    };
 
-    warp::serve(hello).run(([0, 0, 0, 0], 8088)).await;
+    let host = if is_prod {
+        ([0, 0, 0, 0], 10000)
+    } else {
+        ([127, 0, 0, 1], 3000)
+    };
+
+    let hello = warp::path::end().map(|| format!("Hello, world!"));
+    let hello_name = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
+
+    let routes = warp::any().and(hello).or(hello_name);
+
+    println!("serving on port {}", host.1);
+    warp::serve(routes).run(host).await;
 }
