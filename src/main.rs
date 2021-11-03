0f6531cc468bb1b929a5b2d22e02a0858663d36a
Austin Crim <aust.crim@gmail.com>
2021-11-02 22:04:09 -0500

    init

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,8 @@
-use std::env;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    let PORT = match env::var("PORT") {
-        Ok(port) => port,
-        Err(e) => eprintln!("{:?}", e),
-    };
     let hello = warp::path!("hello" / String).map(|name| format!("Hello, {}!", name));
 
-    warp::serve(hello).run(([127, 0, 0, 1], PORT)).await;
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
